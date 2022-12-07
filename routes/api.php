eafa7b6267125bac1056d97e984f8d01d73e1470
brooklyn9916 <rafaspumante@gmail.com>
2022-12-07 19:50:54 +0700

    EHE

--- routes/api.php
+++ routes/api.php
@@ -6,27 +6,33 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::get('/Authors', [AuthorController::class, 'index']);
+Route::get('/Authors/{id}', [AuthorController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');
+});
+
 Route::get('me', [AuthController::class, 'me']);
 
-Route::resource('books', BookController::class)->except(
-    ['create', 'edit']
-);
+// Route::resource('books', BookController::class)->except(
+//     ['create', 'edit']
+// );
 
-Route::resource('authors', AuthorController::class)->except(
-    ['create', 'edit']
-);
+// Route::resource('authors', AuthorController::class)->except(
+//     ['create', 'edit']
+// );
+
+
